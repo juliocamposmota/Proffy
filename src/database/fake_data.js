fdca05c12474c7e8ce2a022ee61fac5549327b1a
Júlio Campos <68956245+juliocamposmota@users.noreply.github.com>
2020-10-07 11:37:20 -0300

    database files added

---
+++ src/database/fake_data.js
@@ -0,0 +1,38 @@
+// dados
+const proffys = [
+    {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "62988117725",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+
+    {
+        name: "Mayk Brito",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp: "(84) 99911-7755",
+        bio: "Se não fosse artista, seria artista.<br><br>.Seja nos quadros, na música ou nos traços, a arte em mim habita. Mas nos traços eu te mostro com quantos condomínios de casa se faz um urbanismo.",
+        subject: "História",
+        cost: "25",
+        weekday: [6],
+        time_from: [720],
+        time_to: [1220]
+    },
+
+    {
+        name: "Gabrielle de Lima",
+        avatar: "https://scontent.fnat1-1.fna.fbcdn.net/v/t1.0-9/59039568_2246842888728888_846426861633273856_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_eui2=AeErvaUuIse6ALdX_u8mtndn3fjr8dsgbvfd-Ovx2yBu95z-G8xfhjppFrMwT0VGdcqCrBUBL3KojZIdTJzPxBdJ&_nc_ohc=-1MrCZirrukAX_LeMwP&_nc_ht=scontent.fnat1-1.fna&oh=c3be963e49b355e92ca0f34ec79777d1&oe=5F54C3EA",
+        whatsapp: "(84) 8862-5174",
+        bio: "Se não fosse artista, seria artista.<br><br>.Seja nos quadros, na música ou nos projetos, a arte em mim habita. Mas é nos projetos eu te mostro com quantos condomínios de casa se faz um urbanismo.",
+        subject: "Artes",
+        cost: "25",
+        weekday: [6],
+        time_from: [720],
+        time_to: [1220]
+    }
+]
